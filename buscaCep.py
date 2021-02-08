63b4181861e96bd6de6b19951146ed1e5df96645
Rodrigo <rodrigonery@hotmail.com.br>
2021-02-08 19:31:04 -0300

    Update

--- buscaCep.py
+++ buscaCep.py
@@ -1,11 +1,21 @@
 import requests
+import re
 
-def buscar():
-    cep = input("Digite o Cep\n")
-    url = f'https://viacep.com.br/ws/{cep}/json/?callback=?'
-    result = requests.get(url)
-    result_dict = result.text
-    print(result_dict)
+cep = input("Digite o Cep: ")
 
+def checar_cep(cep):
+    pattern = r"^\d{5}-\d{3}$"
+    return bool(re.findall(pattern, cep))
 
-buscar()
+checar_cep(cep)
+    
+def buscar_naAPI():
+    if checar_cep(cep):
+        url = f'https://viacep.com.br/ws/{cep}/json/?callback=?'
+        result = requests.get(url)
+        result_dict = result.text
+        print(result_dict)
+    else:
+        print("CEP inválido.")
+    
+buscar_naAPI()
