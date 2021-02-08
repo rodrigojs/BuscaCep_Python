bfe635b7a734fcea19d96f8f6a2780dc66a619b9
Rodrigo <rodrigonery@hotmail.com.br>
2021-02-08 18:41:14 -0300

    Update BuscaCep.py
    O JSON da API dos Correios não aceita qualquer tipo de CEP, então você tem que formatar o CEP com um regex...

--- buscaCep.py
+++ buscaCep.py
@@ -1,11 +1,25 @@
 import requests
+import re
 
-def buscar():
-    cep = input("Digite o Cep\n")
-    url = f'https://viacep.com.br/ws/{cep}/json/?callback=?'
-    result = requests.get(url)
-    result_dict = result.text
-    print(result_dict)
+""""""""
+Essa API não aceita qualquer formato de CEP, então você tem que fazer regex no CEP e forçar o formato..
+""""""""
 
+cep = input("Digite o Cep: ")
 
-buscar()
+def checar_cep(cep):
+    pattern = r"^\d{5}-\d{3}$"
+    return bool(re.findall(pattern, cep))
+
+checar_cep(cep)
+    
+def buscar_naAPI():
+    if checar_cep(cep):
+        url = f'https://viacep.com.br/ws/{cep}/json/?callback=?'
+        result = requests.get(url)
+        result_dict = result.text
+        print(result_dict)
+    else:
+        print("CEP inválido.")
+    
+buscar_naAPI()
