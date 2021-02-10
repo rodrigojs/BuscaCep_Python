0925bb06d5f84aa181dba806e99bbe68b8b3152d
Rodrigo <rodrigonery@hotmail.com.br>
2021-02-10 04:54:29 -0300

    BuscaCep.py

--- buscaCep.py
+++ buscaCep.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import requests
 import re
 
@@ -7,10 +10,10 @@ def checar_cep(cep):
     pattern = r"^\d{5}-\d{3}$"
     return bool(re.findall(pattern, cep))
 
-checar_cep(cep)
+checado = checar_cep(cep)
     
 def buscar_naAPI():
-    if checar_cep(cep):
+    if checado:
         url = f'https://viacep.com.br/ws/{cep}/json/?callback=?'
         result = requests.get(url)
         result_dict = result.text
